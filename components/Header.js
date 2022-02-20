83500860de7dbea8be3b1bb95e22e26683efcf81
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-20 03:53:30 +0200

    Header

--- components/Header.js
+++ components/Header.js
@@ -27,7 +27,7 @@ function Header() {
         <SearchIcon className="hidden h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:inline-flex mr-1 md:mx-2 " />
       </div>
       <div className="flex items-center space-x-4 justify-end text-gray-500">
-        <p cursor-pointer>Become a host</p>
+        <p className="cursor-pointer hidden md:inline-flex">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer " />
         <div className="flex items-center justify-between md:border-2 rounded-full py-2  md:shadow-sm space-x-2 p-2">
           <MenuIcon className="h-6 cursor-pointer " />
