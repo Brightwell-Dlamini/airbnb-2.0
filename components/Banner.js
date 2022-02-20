c911738918bea131905376d91b69926c3aa5a730
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-20 04:10:55 +0200

    jumbotron

--- components/Banner.js
+++ components/Banner.js
@@ -3,8 +3,14 @@ import React from "react";
 
 function Banner() {
   return (
-    <div className="relative h-[300px] sm:h-[480px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] ">
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] ">
       <Image src="/hero.jpg" layout="fill" objectFit="cover" />
+      <div className="absolute top-1/2 w-full text-center ">
+        <p className="text-sm sm:text-lg">Not Sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-4 hover:shadow-xl active:scale-90 transition duration-150">
+          I'm flexible
+        </button>
+      </div>
     </div>
   );
 }
