ef1bc5b123ef9f7ba5389a354dc6164b253a33fc
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-20 03:50:48 +0200

    Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import React from "react";
-import { SearchIcon } from "@heroicons/react/outline";
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  SearchIcon,
+  UserCircleIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10  items-center">
       <div className="relative flex items-center h-10 cursor-pointer my-auto ">
         <Image
           src="/logo.jpg"
@@ -13,15 +18,22 @@ function Header() {
           objectPosition="left"
         />
       </div>
-      <div className="flex items-center justify-between md:border-2 rounded-full py-2 ">
+      <div className="flex items-center justify-between md:border-2 rounded-full py-2  md:shadow-sm">
         <input
           type="text"
           placeholder="Start your search"
-          className="pl-5 bg-transparent outline-none flex-grow"
+          className="pl-5 bg-transparent outline-none flex-grow text-gray-600 text-sm placeholder-gray-400"
         />
-        <SearchIcon className="hidden h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:inline-flex mr-1" />
+        <SearchIcon className="hidden h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:inline-flex mr-1 md:mx-2 " />
       </div>
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p cursor-pointer>Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer " />
+        <div className="flex items-center justify-between md:border-2 rounded-full py-2  md:shadow-sm space-x-2 p-2">
+          <MenuIcon className="h-6 cursor-pointer " />
+          <UserCircleIcon className="h-6 cursor-pointer " />
+        </div>
+      </div>
     </header>
   );
 }
