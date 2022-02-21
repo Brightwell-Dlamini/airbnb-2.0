90b791c43e173e533cf6d1d51e94787c232c66cb
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-21 22:58:23 +0200

    no scrollbar

--- components/Banner.js
+++ components/Banner.js
@@ -6,7 +6,9 @@ function Banner() {
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image src="/hero.jpg" layout="fill" objectFit="cover" alt="hero img" />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not Sure where to go? Perfect.</p>
+        <p className="text-sm sm:text-lg text-white font-semibold">
+          Not Sure where to go? Perfect.
+        </p>
         <button className="text-purple-500 bg-white px-10 py-4 rounded-full shadow-md font-bold my-4 hover:shadow-xl active:scale-90 transition duration-150">
           I&apos;m flexible
         </button>
--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home({ exploreData, cardsData }) {
         <section>
           {" "}
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          <div className="flex space-x-3 overflow-scroll  ">
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide ">
             {" "}
             {cardsData?.map(({ img, title }) => (
               <MediumCard key={img} img={img} title={title} />
