3e0c2a0971a76174c01028a8bc8c6a9e5687f1b2
Brightwell-Dlamini <dlaminibrightwell@gmail.com>
2022-02-21 23:12:12 +0200

    Large Card

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
@@ -33,13 +34,19 @@ export default function Home({ exploreData, cardsData }) {
         <section>
           {" "}
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
-          <div className="flex space-x-3 overflow-scroll scrollbar-hide ">
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {" "}
             {cardsData?.map(({ img, title }) => (
               <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
         </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The greatest Outdoors"
+          description="Wishlists created by airbnb"
+          btnTxt="Get inspired"
+        />
       </main>
     </div>
   );
